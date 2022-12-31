f48b05b860b7bfa7ce0efc206a9275fe0a157a62
Mudasiru Adewale <50289759+whaletb@users.noreply.github.com>
2022-12-31 11:17:22 +0100

    Add files via upload

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,10 @@
-
-
-# Android device tree for TECNO TECNO KC8 (TECNO-KC8)
-
-```
 #
 # Copyright (C) 2022 The Android Open Source Project
 # Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+add_lunch_combo omni_TECNO-KC8-user
+add_lunch_combo omni_TECNO-KC8-userdebug
+add_lunch_combo omni_TECNO-KC8-eng
